94025c061b6da575905a50a4daeff9cd8ceedf76
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-10-05 00:41:00 +0300

    прописал условия

--- js6_ex.js
+++ js6_ex.js
@@ -1,1 +1,26 @@
 'use strict';
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function guessTheNumber(num) {
+    let hiddenNumber = +prompt('Угадай число от 1 до 100');
+    if (hiddenNumber > num) {
+        alert('Загаданное число меньше');
+        guessTheNumber();
+    } else if (hiddenNumber < num) {
+        alert('Загаданное число больше');
+        guessTheNumber();
+    } else if (!isNumber(hiddenNumber)) {
+        alert('Введи число!');
+        guessTheNumber();
+    } else if (hiddenNumber === 5) {
+        alert('You WIN');
+        guessTheNumber();
+    } else {
+        alert('GAME OVER');
+        return;
+    }
+    
+}
+guessTheNumber(5);
